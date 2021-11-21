7968f6ee4c9ce94d35ad3f41460c08178d36b66d
TharinduShantha98 <tharindushantha4@gmail.com>
2021-11-21 15:11:29 -0800

    second commit

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,3 +1,23 @@
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import static javafx.application.Application.launch;
+
+public class AppInitializer extends Application {
+
+    public static void main(String args[]){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent load = FXMLLoader.load(getClass().getResource("views/DashBordForm.fxml"));
+        Scene scene = new Scene(load);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
